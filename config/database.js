56738774c68227b9ebd468a3e7b4389a847d3836
Fernando <84139372+Fernando1403@users.noreply.github.com>
2022-06-15 08:40:24 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 //scripts de conexão
 const conn = async () => {
-    const atlas = await mongoose.connect('[SUA DB]')
+    const atlas = await mongoose.connect('mongodb+srv://Fernando1403:Cluster111@cluster1.uman3.mongodb.net/todo_list')
 }
 
 //exportar as informações para acesso externo
